7e1b617273c4eaa5131d810331d9dcf104ee6b0e
ArielShemesh1999 <arielshemesh1999@gmail.com>
2025-04-24 14:30:23 +0300

    REACT-PROJECT
    פתיחת תקיית פרויקט וסידור עמודים

---
+++ REACT-PROJECT/src/views/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20%' }}>
+      <h1>Home Page</h1>
+    </div>
+  );
+}
+
+export default Home;
